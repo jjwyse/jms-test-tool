d31e061d01312834071edcbb72ebbf6b5d5e683b
jjwyse <josh@cloud-elements.com>
2014-03-26 13:23:59 -0600

    Moving away from spring xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
 import gui.MainFrame;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.apache.camel.CamelContext;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.component.jms.JmsConfiguration;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.jms.ConnectionFactory;
 
 /**
  * TODO - JJW
@@ -11,10 +19,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"camel-context.xml"});
-        CamelContext camelContext = (CamelContext) context.getBean("camelContext");
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("http://localhost:61616");
+
+        PooledConnectionFactory pooledConnectionFactory = new PooledConnectionFactory();
+        pooledConnectionFactory.setMaxConnections(8);
+        pooledConnectionFactory.setConnectionFactory(connectionFactory);
+
+        JmsConfiguration jmsConfig = new JmsConfiguration(pooledConnectionFactory);
+        jmsConfig.setConcurrentConsumers(10);
+
+        JmsComponent jmsComponent = new ActiveMQComponent();
+        jmsComponent.setConfiguration(jmsConfig);
+
+        CamelContext camelContext = new DefaultCamelContext();
+        camelContext.addComponent("jms", ActiveMQComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        camelContext.start();
+
+//        ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"camel-context.xml"});
+//        CamelContext camelContext = (CamelContext) context.getBean("camelContext");
 
         new MainFrame(camelContext.createProducerTemplate());
     }
