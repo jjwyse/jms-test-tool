b30739a8729747e0e11a007287029460fd201392
jjwyse <josh@cloud-elements.com>
2014-03-27 10:15:35 -0600

    Working on send Note POJO

--- src/main/java/com/jjw/jmstesttool/route/CamelRouteBuilder.java
+++ src/main/java/com/jjw/jmstesttool/route/CamelRouteBuilder.java
@@ -1,9 +1,11 @@
 package com.jjw.jmstesttool.route;
 
+import com.jjw.jmstesttool.jaxb.Note;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JaxbDataFormat;
 
 /**
- * TODO - JJW
+ * Configures any listening routes for testing purposes
  *
  * @author jjwyse
  * @version %I%, %G%
@@ -12,6 +14,23 @@ public class CamelRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("jms:queue:testing").to("log:com.jjw.jmstesttool?level=INFO&showAll=true");
+        JaxbDataFormat jaxb = new JaxbDataFormat(true);
+        jaxb.setContextPath("com.jjw.jmstesttool.jaxb");
+
+        // listens on direct:testing, unmarshals POJO and sends to our logging endpoint
+        from("direct:testing").unmarshal(jaxb).to("direct:logging");
+
+        // listens on direct:note, and sends a Note object to our direct:testing endpoint
+        from("direct:note").to("direct:testing");
+
+        // listens on jms:queue:testing and sends to our logging endpoint
+        from("jms:queue:testing").to("direct:logging");
+
+        // listens on direct:logging and then logs a message
+        from("direct:logging").to("log:com.jjw.jmstesttool?level=INFO&showAll=true");
+    }
+
+    private Note createNote() {
+        return new Note();
     }
 }
