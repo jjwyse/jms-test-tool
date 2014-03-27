c2d093c4b978c06263065481c0c47ec49770c2af
jjwyse <josh@cloud-elements.com>
2014-03-27 10:32:11 -0600

    marshal, not umarshal

--- src/main/java/com/jjw/jmstesttool/route/CamelRouteBuilder.java
+++ src/main/java/com/jjw/jmstesttool/route/CamelRouteBuilder.java
@@ -1,6 +1,5 @@
 package com.jjw.jmstesttool.route;
 
-import com.jjw.jmstesttool.jaxb.Note;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JaxbDataFormat;
 
@@ -21,16 +20,12 @@ public class CamelRouteBuilder extends RouteBuilder {
         from("direct:testing").to("direct:logging");
 
         // listens on direct:note, unmarshals object and sends to our logging endpoint
-        from("direct:note").unmarshal(jaxb).to("direct:logging");
+        from("direct:note").marshal(jaxb).to("direct:logging");
 
         // listens on jms:queue:testing and sends to our logging endpoint
         from("jms:queue:testing").to("direct:logging");
 
         // listens on direct:logging and then logs a message
         from("direct:logging").to("log:com.jjw.jmstesttool?level=INFO&showBody=true");
-    }
-
-    private Note createNote() {
-        return new Note();
     }
 }
