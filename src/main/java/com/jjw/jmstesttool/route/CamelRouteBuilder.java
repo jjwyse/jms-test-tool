35039f05881e1df5631384991e6b10f78ee75a01
jjwyse <josh@cloud-elements.com>
2014-03-27 10:26:06 -0600

    Sending NOTE POJO that gets unmarshals to XML and logged

--- src/main/java/com/jjw/jmstesttool/route/CamelRouteBuilder.java
+++ src/main/java/com/jjw/jmstesttool/route/CamelRouteBuilder.java
@@ -17,17 +17,17 @@ public class CamelRouteBuilder extends RouteBuilder {
         JaxbDataFormat jaxb = new JaxbDataFormat(true);
         jaxb.setContextPath("com.jjw.jmstesttool.jaxb");
 
-        // listens on direct:testing, unmarshals POJO and sends to our logging endpoint
-        from("direct:testing").unmarshal(jaxb).to("direct:logging");
+        // listens on direct:testing and sends to our logging endpoint
+        from("direct:testing").to("direct:logging");
 
-        // listens on direct:note, and sends a Note object to our direct:testing endpoint
-        from("direct:note").to("direct:testing");
+        // listens on direct:note, unmarshals object and sends to our logging endpoint
+        from("direct:note").unmarshal(jaxb).to("direct:logging");
 
         // listens on jms:queue:testing and sends to our logging endpoint
         from("jms:queue:testing").to("direct:logging");
 
         // listens on direct:logging and then logs a message
-        from("direct:logging").to("log:com.jjw.jmstesttool?level=INFO&showAll=true");
+        from("direct:logging").to("log:com.jjw.jmstesttool?level=INFO&showBody=true");
     }
 
     private Note createNote() {
